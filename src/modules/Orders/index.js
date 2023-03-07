e30b8d4afc662c2e42d3edc4eadca4ff54027bd0
mustafa <mustafa.bhm3@gmail.com>
2023-03-07 15:30:55 -0700

    setup of firebase config

--- src/modules/Orders/index.js
+++ src/modules/Orders/index.js
@@ -1,59 +1,60 @@
-import orders from '../../assets/data/orders.json';
-import { Card, Table, Tag } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import orders from "../../assets/data/orders.json";
+import { Card, Table, Tag } from "antd";
+import { useNavigate } from "react-router-dom";
+import { db } from "../../firebase";
 
 const Orders = () => {
   const navigate = useNavigate();
-  
+
   const renderOrderStatus = (orderStatus) => {
-    if (orderStatus === 'Accepted') {
-      return <Tag color={'green'}>{orderStatus}</Tag>
+    if (orderStatus === "Accepted") {
+      return <Tag color={"green"}>{orderStatus}</Tag>;
     }
-    if (orderStatus === 'Pending') {
-      return <Tag color={'orange'}>{orderStatus}</Tag>
+    if (orderStatus === "Pending") {
+      return <Tag color={"orange"}>{orderStatus}</Tag>;
     }
-    if (orderStatus === 'Declined') {
-      return <Tag color={'red'}>{orderStatus}</Tag>
+    if (orderStatus === "Declined") {
+      return <Tag color={"red"}>{orderStatus}</Tag>;
     }
   };
 
   const tableColumns = [
     {
-      title: 'Order ID',
-      dataIndex: 'orderID',
-      key: 'orderID',
+      title: "Order ID",
+      dataIndex: "orderID",
+      key: "orderID",
     },
     {
-      title: 'Delivery Address',
-      dataIndex: 'deliveryAddress',
-      key: 'deliveryAddress'
+      title: "Delivery Address",
+      dataIndex: "deliveryAddress",
+      key: "deliveryAddress",
     },
     {
-      title: 'Price',
-      dataIndex: 'price',
-      key: 'price',
-      render: (price) => `${price} $`
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+      render: (price) => `${price} $`,
     },
     {
-      title: 'Status',
-      dataIndex: 'status',
-      key: 'status',
-      render: renderOrderStatus
-    }
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
+      render: renderOrderStatus,
+    },
   ];
 
   return (
-    <Card title={'Orders'} style={{ margin: 20 }}>
+    <Card title={"Orders"} style={{ margin: 20 }}>
       <Table
         dataSource={orders}
         columns={tableColumns}
         rowKey="orderID"
         onRow={(orderItem) => ({
-          onClick: () => navigate(`order/${orderItem.orderID}`)
+          onClick: () => navigate(`order/${orderItem.orderID}`),
         })}
       />
     </Card>
-  )
+  );
 };
 
 export default Orders;
